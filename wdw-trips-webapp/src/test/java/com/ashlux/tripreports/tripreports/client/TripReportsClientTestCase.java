c7b9c3b7bfd6b84a47531c3441a1bf303dea9c3b
Ash Lux <ashlux@gmail.com>
2009-08-31 00:21:18 -0500

    GWT tests. Doesn't work from maven though..

---
+++ wdw-trips-webapp/src/test/java/com/ashlux/tripreports/tripreports/client/TripReportsClientTestCase.java
@@ -0,0 +1,12 @@
+package com.ashlux.tripreports.tripreports.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+abstract public class TripReportsClientTestCase
+    extends GWTTestCase
+{
+    public String getModuleName()
+    {
+        return "com.ashlux.tripreports.tripreports.TripReports";
+    }
+}
---
+++ wdw-trips-webapp/src/test/java/com/ashlux/tripreports/tripreports/client/TripReportsPanelListenerTest.java
@@ -0,0 +1,58 @@
+package com.ashlux.tripreports.tripreports.client;
+
+import com.gwtext.client.widgets.Panel;
+
+public class TripReportsPanelListenerTest
+    extends TripReportsClientTestCase
+{
+    public void testOnActivate_unexpectedPanel()
+    {
+        fail();
+        try
+        {
+            Panel panel = new Panel();
+            new TripReportsPanelListener().onActivate( panel );
+            fail( "Should have had an exception thrown, Panel is of the wrong type." );
+        }
+        catch ( IllegalArgumentException e )
+        {
+            assertTrue( true );
+        }
+    }
+
+    public void testOnActivate_behavior()
+    {
+        final PanelStub panel = new PanelStub();
+        assertFalse( panel.showCalled );
+        TripReportsPanelStub tripReportsPanel = new TripReportsPanelStub();
+        tripReportsPanel.viewPanel = panel;
+        TripReportsPanelListener tripReportsPanelListener = new TripReportsPanelListener();
+        tripReportsPanelListener.onActivate( tripReportsPanel );
+
+        assertTrue( panel.showCalled );
+    }
+
+    public class PanelStub
+        extends Panel
+    {
+        boolean showCalled = false;
+
+        @Override
+        public void show()
+        {
+            super.show();
+            showCalled = true;
+        }
+    }
+
+    public class TripReportsPanelStub
+        extends TripReportsPanel
+    {
+        Panel viewPanel;
+
+        public Panel getViewPanel()
+        {
+            return viewPanel;
+        }
+    }
+}
